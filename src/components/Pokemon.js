1a9a40d99038056ee467c1c9bb6b190f2c6796fe
Thomas Doig <thomas@codeworx.com.au>
2022-05-17 20:49:26 +1000

    Add extra properties to the array

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,34 +1,37 @@
 import { useEffect, useState } from "react"
 
 const Pokemon = ({pokemonNameAndUrl}) => {
-
     const list = pokemonNameAndUrl
     const [pokemon, setPokemon] = useState([])
     
     let pokemonList = []
-useEffect(() => {
-    list?.forEach((item) => {
-        fetch(item.url)
-        .then(response => response.json())
-        .then(data => {
-            let name = data.name
-            let image = data.sprites.front_default
-            pokemonList.push({name: name, image: image})
-            if (pokemonList === 10 ) {
-                setPokemon(pokemonList)
-            }
-            console.log(pokemon)
+    useEffect(() => {
+        list?.forEach((item) => {
+            fetch(item.url)
+            .then(response => response.json())
+            .then(data => {
+                let id = data.id
+                let types = data.types
+                let name = data.name
+                let image = data.sprites.front_default
+                pokemonList.push({id, types, name, image})
+                if (pokemonList.length === 20) {
+                    setPokemon(pokemonList)
+                    console.log(pokemon)
+                }
+            })
         })
-    })
-}, [list])
+    }, [list])
 
-    let critter = pokemon.map(pokemon => {
-        return <li>{pokemon.name}<img src={pokemon.image}/></li>
+    let pokemons = pokemon.map(pokemon => {
+        return <li style={{listStyleType: 'none'}}>
+            <img src={pokemon.image}/><span><h1 style={{display: 'inline'}}>{pokemon.name}</h1></span>
+        </li>
     })
 
     return (
         <div>
-            <ul>{critter}</ul>
+            <ul>{pokemons}</ul>
         </div>
     )
         

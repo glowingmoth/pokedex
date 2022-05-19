1f8b38b83e2591c59097ce69d1d904fcbb3fb9fc
Thomas Doig <thomas@codeworx.com.au>
2022-05-19 14:08:12 +1000

    A lot of random stuff related to styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Pokemon from './components/Pokemon';
 function App() {
   const [data, setData] = useState([])
   // ?limit=60&offset=60
-  const [limit, setLimit] = useState(20)
-  const [offset, setOffset] = useState(20)
+  const [limit, setLimit] = useState(151)
+  const [offset, setOffset] = useState(0)
    
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
@@ -14,8 +14,14 @@ function App() {
       .then(data => setData(data.results))
   }, [])
 
+  const forward = () => {
+    setOffset(() => offset + limit)
+  }
+
   return (
     <div>
+      <button>Backward</button>
+      <button onClick={forward}>Forward</button>
       <h1>Pokédex</h1>
       <Pokemon pokemonNameAndUrl={data} />
     </div>

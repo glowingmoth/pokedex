e152441511c5dc434aacf4088e38d5d18c89db21
Thomas Doig <thomas@codeworx.com.au>
2022-06-11 22:23:59 +1000

    Format tabs

---
+++ src/components/Pokeball.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Pokeball = () => {
+  return (
+    <div class="item">
+      <div class="ball"></div>
+      <div class="half-ball-top"></div>
+      <div class="half-ball-bottom"></div>
+      <div class="big-button"></div>
+      <div class="small-button"></div>
+      <div class="horizon"></div>
+    </div>
+  );
+};
+
+export default Pokeball;
